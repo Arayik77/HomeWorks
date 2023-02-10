35a01c854866c4bbd0a9adee0b91001f07d6c485
Arayik <arayik_grigoryan_97@mail.ru>
2023-02-11 02:27:24 +0400

    home work 1

---
+++ HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HomeWork1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(9>8)
+            {
+                Console.WriteLine("yes!!!");
+            } else
+            {
+                Console.WriteLine("no!!!");
+            }
+        }
+    }
+}
